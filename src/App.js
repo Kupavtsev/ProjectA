2fad4eab1d86ebebcce00b663c723686e5d6bba8
Oleg Kupavtsev <olegkupavtsev@gmail.com>
2021-04-10 00:17:51 +0300

    responsive not DRY

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import './App.css';
+import { useMediaQuery } from 'react-responsive';
 
 // My Components
 // Structure
@@ -21,31 +22,177 @@ import { YMInitializer } from 'react-yandex-metrika';
 
 
 function App() {
-  return (
-    <div className='app-wrapper'>
-      <BrowserRouter>
-        <Header />
-        {/* <Content /> */}
-        
 
-        <div className="app-wrapper-content">
-          <Switch>
-            <Route exact path="/" component={Content} />
-            <Route exact path="/tents" component={Tents} />
-            <Route exact path="/articles" component={Articles} />
-            <Route exact path="/containers" component={Containers} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/tents-euro" component={TentsEuro} />
-            <Route exact path="/semitrailers" component={Semitrailers} />
-            <Route exact path="/semitrailers-cont" component={SemitrailersCont} />
-            <Route exact path="/trals" component={Trals} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-      <Footer />
-      <YMInitializer accounts={[75168874]} />
-    </div>
-  );
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-width: 1224px)'
+  })
+  const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+  const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
+
+  if (isDesktopOrLaptop) {
+    return (
+      <div className='app-wrapper'>
+        <BrowserRouter> 
+          <Header /> 
+          {/* <Content /> */}
+          <div className="app-wrapper-content">
+            <Switch>
+              <Route exact path="/" component={Content} />
+              <Route exact path="/tents" component={Tents} />
+              <Route exact path="/articles" component={Articles} />
+              <Route exact path="/containers" component={Containers} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/tents-euro" component={TentsEuro} />
+              <Route exact path="/semitrailers" component={Semitrailers} />
+              <Route exact path="/semitrailers-cont" component={SemitrailersCont} />
+              <Route exact path="/trals" component={Trals} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+        <Footer />
+        <YMInitializer accounts={[75168874]} />
+        
+      </div>
+    );
+  }
+
+  if (isBigScreen) {
+    return (
+      <div className='app-wrapper'>
+        <BrowserRouter> 
+          <Header /> 
+          {/* <Content /> */}
+          <div className="app-wrapper-content">
+            <Switch>
+              <Route exact path="/" component={Content} />
+              <Route exact path="/tents" component={Tents} />
+              <Route exact path="/articles" component={Articles} />
+              <Route exact path="/containers" component={Containers} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/tents-euro" component={TentsEuro} />
+              <Route exact path="/semitrailers" component={Semitrailers} />
+              <Route exact path="/semitrailers-cont" component={SemitrailersCont} />
+              <Route exact path="/trals" component={Trals} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+        <Footer />
+        <YMInitializer accounts={[75168874]} />
+        
+      </div>
+    );
+  }
+
+  if (isTabletOrMobile) {
+    return (
+      <div className='app-wrapper'>
+        <BrowserRouter> 
+          <Header /> 
+          {/* <Content /> */}
+          <div className="app-wrapper-content">
+            <Switch>
+              <Route exact path="/" component={Content} />
+              <Route exact path="/tents" component={Tents} />
+              <Route exact path="/articles" component={Articles} />
+              <Route exact path="/containers" component={Containers} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/tents-euro" component={TentsEuro} />
+              <Route exact path="/semitrailers" component={Semitrailers} />
+              <Route exact path="/semitrailers-cont" component={SemitrailersCont} />
+              <Route exact path="/trals" component={Trals} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+        <Footer />
+        <YMInitializer accounts={[75168874]} />
+        
+      </div>
+    );
+  }
+
+  if (isPortrait) {
+    return (
+      <div className='app-wrapper'>
+        <BrowserRouter> 
+          <Header /> 
+          {/* <Content /> */}
+          <div className="app-wrapper-content">
+            <Switch>
+              <Route exact path="/" component={Content} />
+              <Route exact path="/tents" component={Tents} />
+              <Route exact path="/articles" component={Articles} />
+              <Route exact path="/containers" component={Containers} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/tents-euro" component={TentsEuro} />
+              <Route exact path="/semitrailers" component={Semitrailers} />
+              <Route exact path="/semitrailers-cont" component={SemitrailersCont} />
+              <Route exact path="/trals" component={Trals} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+        <Footer />
+        <YMInitializer accounts={[75168874]} />
+        
+      </div>
+    );
+  }
+
+  if (isRetina) {
+    return (
+      <div className='app-wrapper'>
+        <BrowserRouter> 
+          <Header /> 
+          {/* <Content /> */}
+          <div className="app-wrapper-content">
+            <Switch>
+              <Route exact path="/" component={Content} />
+              <Route exact path="/tents" component={Tents} />
+              <Route exact path="/articles" component={Articles} />
+              <Route exact path="/containers" component={Containers} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/tents-euro" component={TentsEuro} />
+              <Route exact path="/semitrailers" component={Semitrailers} />
+              <Route exact path="/semitrailers-cont" component={SemitrailersCont} />
+              <Route exact path="/trals" component={Trals} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+        <Footer />
+        <YMInitializer accounts={[75168874]} />
+        
+      </div>
+    );
+  }
+
+  else {
+    return (
+      <div className='app-wrapper'>
+        <BrowserRouter> 
+          <Header /> 
+          {/* <Content /> */}
+          <div className="app-wrapper-content">
+            <Switch>
+              <Route exact path="/" component={Content} />
+              <Route exact path="/tents" component={Tents} />
+              <Route exact path="/articles" component={Articles} />
+              <Route exact path="/containers" component={Containers} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/tents-euro" component={TentsEuro} />
+              <Route exact path="/semitrailers" component={Semitrailers} />
+              <Route exact path="/semitrailers-cont" component={SemitrailersCont} />
+              <Route exact path="/trals" component={Trals} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+        <Footer />
+        <YMInitializer accounts={[75168874]} />
+        
+      </div>
+    );
+  }
+
 }
 
 export default App;
