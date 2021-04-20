1d117844002a6ea60d31117faa166dddf893194d
Oleg Kupavtsev <olegkupavtsev@gmail.com>
2021-04-20 22:24:55 +0300

    CORS deploy

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS #comment this on deployment
+# from flask_cors import CORS #comment this on deployment
 
 import os
 
@@ -11,7 +11,7 @@ STATIC_FOLDER = os.path.join(APP_DIR, '../static/build/static') # Where your web
 TEMPLATE_FOLDER = os.path.join(APP_DIR, '../static/build') # Where your index.html file is located
 
 app = Flask(__name__, static_folder=STATIC_FOLDER, template_folder=TEMPLATE_FOLDER)
-CORS(app) #comment this on deployment
+# CORS(app) #comment this on deployment
 app.config.from_object('app.config.TestingConfig')
 
 db = SQLAlchemy(app)
