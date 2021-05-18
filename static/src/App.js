4df34413518bbd2b2e6b4a65c0d529c248b028e4
Oleg Kupavtsev <olegkupavtsev@gmail.com>
2021-05-18 14:34:16 +0300

    looking for mobile SPAM

--- static/src/App.js
+++ static/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { useMediaQuery } from 'react-responsive';
+// import { useMediaQuery } from 'react-responsive';
 
 // My Components
 
@@ -9,37 +9,40 @@ import BaseLayout from "./components/BaseLayout";
 
 function App() {
 
-  const isDesktopOrLaptop = useMediaQuery({
-    query: '(min-width: 1224px)'
-  })
-  const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
-  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
-  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
-  const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
+  // const isDesktopOrLaptop = useMediaQuery({
+  //   query: '(min-width: 1224px)'
+  // })
+  // const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
+  // const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+  // const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+  // const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
 
-  if (isDesktopOrLaptop) {
-    return <BaseLayout />
-  }
+  // if (isDesktopOrLaptop) {
+  //   return <BaseLayout />
+  // }
 
-  if (isBigScreen) {
-    return <BaseLayout />
-  }
+  // if (isBigScreen) {
+  //   return <BaseLayout />
+  // }
 
-  if (isTabletOrMobile) {
-    return <BaseLayout />
-  }
+  // if (isTabletOrMobile) {
+  //   return <BaseLayout />
+  // }
 
-  if (isPortrait) {
-    return <BaseLayout />
-  }
+  // if (isPortrait) {
+  //   return <BaseLayout />
+  // }
 
-  if (isRetina) {
-    return <BaseLayout />
-  }
+  // if (isRetina) {
+  //   return <BaseLayout />
+  // }
 
-  else {
-    return <BaseLayout />
-  }
+  // else {
+  //   return <BaseLayout />
+  // }
+
+
+  return <BaseLayout />
 
 }
 
